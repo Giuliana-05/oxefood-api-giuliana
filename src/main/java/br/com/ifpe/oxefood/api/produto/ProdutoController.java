3d9388c58e5dc535e9a7e51370fa460581eba8db
MaryGiu <mariagiuliana653@gmail.com>
2025-09-23 11:44:01 -0300

    ligação

---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -0,0 +1,40 @@
+package br.com.ifpe.oxefood.api.produto;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.api.produto.ProdutoRequest;
+import br.com.ifpe.oxefood.api.produto.ProdutoRequest;
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
+
+// end point de cliente (rotas)
+// http://localhost:8080/api/cliente/
+
+@RestController
+@RequestMapping("/api/produto") // url para acessar funções
+@CrossOrigin
+public class ProdutoController {
+    
+    @Autowired
+   private ProdutoService produtoService;
+
+  //@PostMapping(path="/cadastrar") para acessar outro post
+   @PostMapping
+   public ResponseEntity<Produto> save(@RequestBody ProdutoRequest request) {
+
+       Produto produto = produtoService.save(request.build());
+       return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
+   }
+    
+
+}
---
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -0,0 +1,23 @@
+package br.com.ifpe.oxefood.modelo.entregador;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class EntregadorService {
+    
+    @Autowired // injetar uma depedencia auto / inicializar um atributo de uma classe
+   private EntregadorRepository repository;
+
+   @Transactional // criar um bloco transacional 
+   public Entregador save(Entregador entregador) {
+
+       entregador.setHabilitado(Boolean.TRUE);
+       return repository.save(entregador);
+   }
+
+}
